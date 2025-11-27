d8884e8476e1007bbb7afe7a9832011f686b48b4
trashcore-dev <trashcoreclient@gmail.com>
2025-11-27 12:09:25 +0300

    Create chat.js

---
+++ routes/chat.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { getChatResponse } = require('../controllers/chatController');
+const router = express.Router();
+
+router.post('/', getChatResponse);
+
+module.exports = router;
