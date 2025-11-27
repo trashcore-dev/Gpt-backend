8292df75946cb790d5cfed18223a5cb72a96e893
trashcore-dev <trashcoreclient@gmail.com>
2025-11-27 13:09:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ app.use((err, req, res, next) => {
     res.status(500).json({ error: 'Something went wrong!' });
 });
 
+// 404 handler
+app.use('*', (req, res) => {
+    res.status(404).json({ error: 'Route not found' });
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
